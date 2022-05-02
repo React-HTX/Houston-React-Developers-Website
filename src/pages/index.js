6aaf29ab687ad751ef74ae6e4dfdfb216d973e90
Tracy Tucker <tracytuckeremail@gmail.com>
2022-05-02 08:39:15 -0500

    Adding responsive hero comp with icon, message and next event btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
-import Event from "../components/Event"
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
-      <Event />
     </Layout>
   )
 }
